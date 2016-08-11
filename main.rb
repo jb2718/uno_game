96146cc041e5194eeb887f7e043ec8c9c8891ee1
jb2718-SE <jbattle2718@gmail.com>
2016-08-11 14:13:31 -0400

    added image of uno deck to files

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Game
   def initialize  # this handles the "start" verb
     # players start with 7 cards each
@@ -34,12 +36,23 @@ class Player
 end
 
 class Card
-  # these are more like attributes of a card: Color, Number, Symbol
+  SYMBOLS = [:skip, :reverse, :draw_two, :draw_four, :wild]
+  COLORS = [:red, :blue, :green, :yellow]
+  NUMBERS = (0..9).to_a
   # these are types of cards: Discard card, Wild card
+  attr_accessor :color, :number, :symbol
+  
   def initialize
+    @color = nil
+    @number = nil
+    @symbol = nil
   end
 
-  def match?(card)
+  def match?(other_card)
+  end
+
+  def to_s
+    {color: @color, number: @number, symbol: @symbol}
   end
 end
 
@@ -53,6 +66,24 @@ class Match
 end
 
 class Deck
+  def initialize
+    @deck = []
+  end
+
+  def new_deck
+    Card::COLORS.each do |color|
+      Card::NUMBERS.each do |num|
+        card = Card.new
+        card.color = color
+        card.number = num
+        @deck << card
+      end
+    end
+  end
+
+  def to_s
+    @deck.each {|card| puts "#{card.to_s}" }
+  end
 end
 
 class Flow
@@ -71,3 +102,7 @@ end
 
 # Loose Verb:
 # command --> should this be something a card does?  or will the game tell the player to do it?
+
+lucky_deck = Deck.new
+lucky_deck.new_deck
+puts "#{lucky_deck}"
