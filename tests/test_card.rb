22fda664b2c4f7888867337a02227520b425ed6e
jb2718-SE <jbattle2718@gmail.com>
2016-09-26 12:03:00 -0400

    added files for test and rearranged project file structure to separate folders for tests/code base

---
+++ tests/test_card.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+require_relative '../lib/card'
+
+class CardTest < Minitest::Test
+  def test_card_stuff
+  end
+end
---
+++ tests/test_deck.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+require_relative '../lib/deck'
+
+# total number of cards in new deck should be 109?
